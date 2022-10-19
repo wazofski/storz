f52d7174a9e087dc632c3b6bcdfd2a73229273ab
wazofski <aram.aghababyan@gmail.com>
2022-10-18 20:29:06 -0700

    todos and scripts

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -6,6 +6,7 @@ go test -ginkgo.v -args store=0
 go test -ginkgo.v -args store=1
 go test -ginkgo.v -args store=3
 go test -ginkgo.v -args store=4
+go test -ginkgo.v -args store=5
 
 trap "kill 0" EXIT
 
